4124fe35b0dc1bcd1cbd673ec46f0ba93df378da
Giacomo <giacan20@gmail.com>
2021-05-13 23:13:59 +0200

    boh

--- Elaborato/js/game.js
+++ Elaborato/js/game.js
@@ -4,14 +4,17 @@ let games = document.getElementById("games")
 let Games = []
 
 function getFromLocalStorage() {
-    var game = JSON.parse(localStorage.getItem("games"));
-    /*
-    document.getElementById("header").innerText = game.name
-    document.getElementById("rating") = game.rating
-    document.getElementById("")
-    document.getElementById("")
-    */
-    document.getElementById("header").innerText = "ciao"
+  var game = JSON.parse(localStorage.getItem("games"))
+  
+  document.getElementById("header").innerText = game.name
+  document.getElementById("rating").innerText = "Valutazione: " + game.rating
+  let description = game.description
+  var array = description.split("<h3>Plot and setting</h3>")
+  document.getElementById("description1").insertAdjacentHTML('afterbegin', array[0])
+  document.getElementById("description2").insertAdjacentHTML('beforeend', array[1])
+  //document.getElementById("")
+  
+  //document.getElementById("header").innerText = "ciao"
 }
 
 getFromLocalStorage()
