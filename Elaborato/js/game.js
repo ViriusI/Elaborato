34cdc3f40162b6426efeac584e23ba381c536771
Giacomo <giacan20@gmail.com>
2021-05-20 17:44:38 +0200

    a

--- Elaborato/js/game.js
+++ Elaborato/js/game.js
@@ -1,7 +1,7 @@
 /* get da localstorage del gioco e output informazioni
 */
-let games = document.getElementById("games")
-let Games = []
+//let games = document.getElementById("games")
+//let Games = []
 
   var game = JSON.parse(localStorage.getItem("games"))
 
@@ -13,7 +13,10 @@ let Games = []
   document.getElementById("descrizione").insertAdjacentHTML('beforeend', description)
   var div = document.getElementById("h");
   div.style.background = `linear-gradient(to bottom, transparent 0%, white 100%), url(${game.background_image}) no-repeat center top`;
-  //for(var i = 0; i < generi.lenght; i++ )
-  //document.getElementById("")
+  for(var i = 0; i < generi.lenght; i++ )
+    var string = generi[i].name
+    var genere
+    genere = genere.concat(", ", string)
+  document.getElementById("generi").innerText = genere
   
   //document.getElementById("header").innerText = "ciao"
