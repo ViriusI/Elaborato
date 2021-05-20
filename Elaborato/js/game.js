d6ba99d2fd65827e27f6463988ce239dc4064221
Giacomo <giacan20@gmail.com>
2021-05-20 18:07:56 +0200

    a

--- Elaborato/js/game.js
+++ Elaborato/js/game.js
@@ -3,20 +3,23 @@
 //let games = document.getElementById("games")
 //let Games = []
 
-  var game = JSON.parse(localStorage.getItem("games"))
+var game = JSON.parse(localStorage.getItem("games"))
 
-  var generi = game.genres
-  
-  document.getElementById("header").innerText = game.name
-  document.getElementById("rating").innerText = "Valutazione: " + game.rating
-  let description = game.description
-  document.getElementById("descrizione").insertAdjacentHTML('beforeend', description)
-  var div = document.getElementById("h");
-  div.style.background = `linear-gradient(to bottom, transparent 0%, white 100%), url(${game.background_image}) no-repeat center top`;
-  for(var i = 0; i < generi.lenght; i++ )
-    var string = JSON.stringify(generi[i].name)
-    var genere
-    genere = genere.concat(", ", string)
-  document.getElementById("generi").innerText = genere
-  
-  //document.getElementById("header").innerText = "ciao"
+var generi = game.genres
+
+console.log("ciao")
+
+document.getElementById("header").innerText = game.name
+document.getElementById("rating").innerText = "Valutazione: " + game.rating
+let description = game.description
+document.getElementById("descrizione").insertAdjacentHTML('beforeend', description)
+var div = document.getElementById("h");
+div.style.background = `linear-gradient(to bottom, transparent 0%, white 100%), url(${game.background_image}) no-repeat center top`;
+for(var i = 0; i < generi.lenght; i++ ){
+  var string = JSON.stringify(generi[i].name)
+  var genere
+  genere = genere.concat(", ", string)
+}
+document.getElementById("generi").innerText = genere
+
+//document.getElementById("header").innerText = "ciao"
